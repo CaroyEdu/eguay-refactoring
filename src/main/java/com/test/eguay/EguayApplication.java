640ad3100a616533791de5541c702a70ef46338c
royedu <48225409+RoyEdu@users.noreply.github.com>
2022-05-16 11:55:18 +0200

    Proyecto Inicial

---
+++ src/main/java/com/test/eguay/EguayApplication.java
@@ -0,0 +1,13 @@
+package com.test.eguay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EguayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EguayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/eguay/EguayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.eguay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EguayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
